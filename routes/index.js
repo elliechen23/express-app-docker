d68a913715b8a9a6b5b2519817a468d3c26daf3d
Ellie Chen <elliechen@example.com>
2017-11-11 13:47:54 +0800

    2017-11-11 update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 //http://localhost:3000/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',users:[{'name':'Jack','age':35,'email':'Jack@gmail.com'},{'name':'Mary','age':28,'email':'Mary@gmail.com'},{'name':'Tom','age':39,'email':'Tom@gmail.com'}] });
+  res.render('index', { title: 'Microservice Demo',users:[{'name':'Jack','age':35,'email':'Jack@gmail.com'},{'name':'Mary','age':28,'email':'Mary@gmail.com'},{'name':'Tom','age':39,'email':'Tom@gmail.com'}] });
 });
 
 module.exports = router;
