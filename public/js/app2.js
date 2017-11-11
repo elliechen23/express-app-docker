8413b6c0f89807e150e7bb49d1bcaf2dd5a48ccb
Ellie Chen <elliechen@example.com>
2017-11-11 13:27:54 +0800

    2017-11-11 update

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,15 +1,19 @@
 // 等候 HTML 文件完成載入
 $(document).ready(function(){
     initSubmitForm();
+    doSubmitForm();
   });
   //for localhost
   //var sitelink  = "http://127.0.0.1:8080";
+  var apilink = "/api2/books/";
+  var upd_flg = false;
+  var UPD_ID="";
   //for docker ip
   var sitelink  = "http://192.168.99.100:8080";
   var initSubmitForm = function() {
       // 使用 ajax() 來呼叫 REST API
       $.ajax({
-          url: sitelink + '/api2/books',
+          url: sitelink + apilink,
           type: "GET",
           dataType: "json",
           complete: function(data, textStatus, jqXHR) {
@@ -33,12 +37,143 @@ $(document).ready(function(){
               + "<td>" + data[i].author + "</td>"
               + "<td>" + data[i].price + "</td>"
               + "<td>" 
-              + '<button  class="btn btn-primary" type="button"><span  class="glyphicon glyphicon-pencil"></span></button>' 
+              + "<button  class='btn btn-primary' type='button' onclick='upd_click(" + i + ");'><span  class='glyphicon glyphicon-pencil'></span></button>"
               + "</td><td>" 
-              + '<button  class="btn btn-primary" type="button"><span  class="glyphicon glyphicon-trash"></span></button>' 
+              + "<button  class='btn btn-primary' type='button' onclick='del_click(" + i + ");'><span  class='glyphicon glyphicon-trash'></span></button>"
               + "</td>"
               +"</tr>";
 
           $('#book_message').append(htmlCode);
       }
   }
+
+  
+    function upd_click(rowsid){
+        //Javascript 取得table 中TD的值 
+        var VAL=document.getElementById("tb").rows[rowsid+1].cells[0]; 
+        var ID_VAL=VAL.innerHTML;
+        UPD_ID=ID_VAL;
+        getFormIDValue(ID_VAL);
+        upd_flg=true;
+    }
+    
+    var getFormIDValue= function(ID_VAL) {
+        // 使用 ajax() 來呼叫 REST API
+        $.ajax({
+            url: sitelink + apilink + ID_VAL,
+            type: "GET",
+            dataType: "json",
+            complete: function(data, textStatus, jqXHR) {
+                console.log(textStatus);
+                //alert(textStatus);
+            },
+            success: function (data, textStatus, jqXHR) {
+              console.log(data);  
+              dataMapping2(data);
+            }
+        });
+        return false;
+    };
+
+    var dataMapping2=function(data){
+      var json=JSON.stringify(data);
+        JSON.parse(json, function(k, v) {
+          if (k=="name"){
+            $("#name").val(v); 
+          }
+          if (k=="author"){
+            $("#author").val(v);
+          }
+          if (k=="price"){
+            $("#price").val(v);
+          }
+         
+        });
+    }
+
+    function del_click(rowsid){
+        //Javascript 取得table 中TD的值 
+        var td=document.getElementById("tb").rows[rowsid+1].cells[0]; 
+        var ID_VAL=td.innerHTML;
+        var tr = td.parentNode; // td元素的父節點 = tr
+        delFormIDValue(ID_VAL);
+        tr.parentNode.removeChild(tr); //利用tr的父節點刪除tr
+        upd_flg=false;
+    }
+
+    var delFormIDValue= function(ID_VAL) {
+        // 使用 ajax() 來呼叫 REST API
+        $.ajax({
+            url: sitelink + apilink + ID_VAL,
+            type: "DELETE",
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            complete: function(data, textStatus, jqXHR) {
+                console.log(textStatus);
+            },
+            success: function (data, textStatus, jqXHR) {
+              console.log(data);  
+              alert(data);
+            }
+        });
+        return false;
+    };
+
+    var doSubmitForm = function(){
+        
+        $("#okbtn").click(function(){
+          var name=$("#name").val();
+          var author=$("#author").val();
+          var price=$("#price").val();
+          var data='{"name":"' + name + '",' +
+          '"author":"' + author + '",' + 
+          '"price":' + price + '}';
+          if(upd_flg==false){
+            postFormIDValue(data);
+          }
+          else if(upd_flg=true){
+            putFormIDValue(data);
+          }
+          upd_flg=false;
+        });
+    };
+
+    var postFormIDValue= function(data) {
+        // 使用 ajax() 來呼叫 REST API
+        $.ajax({
+            url: sitelink + apilink ,
+            type: "POST",
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            complete: function(data, textStatus, jqXHR) {
+                console.log(textStatus);
+            },
+            success: function (data, textStatus, jqXHR) {
+              console.log(data);  
+              alert(data);
+            }
+        });
+        return false;
+    };
+
+    var putFormIDValue= function(data) {
+        // 使用 ajax() 來呼叫 REST API
+        $.ajax({
+            url: sitelink + apilink + UPD_ID,
+            type: "PUT",
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            complete: function(data, textStatus, jqXHR) {
+                console.log(textStatus);
+            },
+            success: function (data, textStatus, jqXHR) {
+              console.log(data); 
+              alert(data); 
+            }
+        });
+        return false;
+    };
+ 
+    
